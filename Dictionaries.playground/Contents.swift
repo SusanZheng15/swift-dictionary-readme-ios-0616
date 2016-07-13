ed66d93beab63bf2a85ade02b38c344571396801
SusanZheng15 <zheng.susan12@gmail.com>
2016-07-13 09:53:12 -0400

    Done

--- Dictionaries.playground/Contents.swift
+++ Dictionaries.playground/Contents.swift
@@ -1,4 +1,5 @@
-var favoriteWords = [
+var favoriteWords =
+[
     "rigmarole": "a lengthy and complicated procedure",
     "gobbledygook": "language that is meaningless; nonsense",
     "lollygag": "spend time aimlessly",
@@ -11,15 +12,21 @@ var definition = favoriteWords["rigmarole"]
 
 var nonexistentDefinition = favoriteWords["strategery"]
 
-if let definition = definition {
+if let definition = definition
+{
     print("The definition of 'rigmarole' is '\(definition)'")
-} else {
+}
+else
+{
     print("'rigmarole' isn't a word!")
 }
 
-if let definition = nonexistentDefinition {
+if let definition = nonexistentDefinition
+{
     print("The definition of 'strategery' is '\(definition)'")
-} else {
+}
+else
+{
     print("'strategery' isn't a word")
 }
 
